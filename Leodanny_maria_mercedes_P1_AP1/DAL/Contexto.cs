2a2b5ebc39a14c6aef213afef0dc177f865c5605
Leodanny Maria Mercedes <leodanny_maria@ucne.edu.do>
2025-02-17 14:37:46 -0400

    cree la carpeta DAL

---
+++ Leodanny_maria_mercedes_P1_AP1/DAL/Contexto.cs
@@ -0,0 +1,8 @@
+﻿namespace Leodanny_maria_mercedes_P1_AP1.Contexto
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+        public DbSet<Modelo> Modelo { get; set; }
+    }
+}
