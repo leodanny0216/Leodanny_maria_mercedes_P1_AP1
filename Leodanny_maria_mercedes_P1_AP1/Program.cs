a80c9aa4f5af74add33ef3c44bcfe191ce2a13e0
Leodanny Maria Mercedes <leodanny_maria@ucne.edu.do>
2025-02-18 00:06:29 -0400

    hice la inyeccion

--- Leodanny_maria_mercedes_P1_AP1/Program.cs
+++ Leodanny_maria_mercedes_P1_AP1/Program.cs
@@ -1,11 +1,22 @@
 using Leodanny_maria_mercedes_P1_AP1.Components;
+using Leodanny_maria_mercedes_P1_AP1.DAL;
+using Leodanny_maria_mercedes_P1_AP1.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-builder.Services.AddScoped<ModeloServices>();
+builder.Services.AddScoped<ServicesReportes>();
+builder.Services.AddBlazorBootstrap();
+
+var connectionString = builder.Configuration.GetConnectionString("SqlConStr")
+    ?? throw new InvalidOperationException("Connection string 'SqlConStr' not found.");
+
+// ?? Cambio importante: Usar IDbContextFactory para Blazor Server
+builder.Services.AddDbContextFactory<Contexto>(options =>
+    options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
